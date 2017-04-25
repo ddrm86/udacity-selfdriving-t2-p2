2563a5de62b16615268068591afa2990b897e3b7
David del Río Medina <ddrm86@gmail.com>
2017-04-25 18:57:52 +0200

    Add RMSE calculation

--- src/tools.cpp
+++ src/tools.cpp
@@ -4,6 +4,8 @@
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
+using std::cout;
+using std::endl;
 
 Tools::Tools() {}
 
@@ -11,8 +13,22 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+  unsigned int n_est = estimations.size();
+  if ((n_est == 0) || (n_est != ground_truth.size())) {
+    cout << "Zero estimations or estimations and ground truth data of different sizes" << endl;
+    return rmse;
+  }
+  
+  for (int i=0; i<n_est; i++) {
+    VectorXd difference = estimations[i] - ground_truth[i];
+    VectorXd quad_dif = difference.array() * difference.array();
+    rmse += quad_dif;
+  }
+  rmse = rmse / n_est;
+  rmse = rmse.array().sqrt();
+  
+  return rmse;
 }
+
